2b950306b7d4501e2c54e33310d7dbe9a7efe6f9
jmgarcia <jmgarcia@prolesys.com>
2025-10-06 11:34:35 -0400

    Detalle:
    Corrección de Configuraciones y prueba de componente guardian

--- src/plugins/router/index.jsx
+++ src/plugins/router/index.jsx
@@ -1,16 +1,30 @@
-import { createBrowserRouter } from 'react-router-dom'
-import App from '@/App'
-import Home from '@/views/auth/Home'
-
+import { createBrowserRouter } from 'react-router-dom';
+import App from '@/App';
+import AuthGuard from '@/components/AuthGuard'; 
+import Home from '@/views/auth/Home/index.jsx';
+import Login from '@/views/public/Login/index.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <App />, // App es el envoltorio (layout) principal
     children: [
-      { path: '', element: <Home /> },
+      {
+        path: '/',
+        element: <AuthGuard />, // AuthGuard es el componente protector
+        children: [
+          {
+            path: '', // Ruta raíz: "/"
+            element: <Home />, // La Home solo se carga si AuthGuard lo permite
+          },
+        ],
+      },
+      {
+        path: 'login', // Ruta: "/login"
+        element: <Login />, // Login es una ruta pública, pero AuthGuard aún la protege contra accesos de usuarios logueados
+      },
     ],
   },
-])
+]);
 
-export default router
+export default router;
